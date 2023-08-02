5a68c84cf5fbbea5079f776132fc1514e267184b
Hector <hecse71@gmail.com>
2023-08-02 20:25:43 -0300

    "Primer commit"

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Header from './components/Header'
+import './App.css'
+import './styles/Header.css'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <main>
+        <p>Hero más Call to Action</p>
+      </main>
+      <footer>
+        <p>Acá va el footer</p>
+        <a href="">About us</a>
+      </footer>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <header className='header-container'>
+            <h2 className='header-title'>My Tynerary </h2>
+            <a href="">Home</a>
+            <a href="">Cities</a>
+        </header>
+    )
+}
+
+export default Header
