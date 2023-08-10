9f44dcaf068068c2929ed9d7e43fe038f1c36584
Hector <hecse71@gmail.com>
2023-08-10 01:26:25 -0300

    Corregido el footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
 
     return (
         <header className="absolute inset-x-0 top-0">
-            <nav className="flex items-center justify-between p-4 lg:px-8" aria-label="Global">
+            <nav className="flex items-center justify-between p-4 lg:px-8" >
                 <div className="flex items-center lg:flex-1">
                     <a href="/#" className="-m-1.5 p-1.5">
                         <span className="sr-only">MyTinerary</span>
